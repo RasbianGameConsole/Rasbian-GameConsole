5f8148c6a394e6b2116790c44476581a2e132fb9
Edward Hubbard <edwardhubbard@Edwards-MacBook-Air.local>
2018-12-20 01:59:20 -0600

    makefile

--- src/drivers/iPhoneModule.c
+++ src/drivers/iPhoneModule.c
@@ -1,17 +1,27 @@
 #include <linux/module.h>
-#include <linux/kernel.h>
+#include <linux/init.h>
 #include <linux/usb.h>
 
 static struct usb_device_id id_table[] = {
     {USB_DEVICE(0x05AC,0x12a8)}, //iPhone5/5C/5S/6
+    {} 
 }
 
-static void i_probe(){
+static void i_probe(struct usb_interface *interface,struct usb_device_id *id){
 
 }
 
 static void i_disconnect(){
 
+}
+
+static int i_init(void){
+    printk(KERN_ALERT "IPHONE INIT");
+    return 0;
+}
+
+static void  i_exit(void){
+    printk(KERN_ALERT "IPHONE EXIT");
 }
 
 static struct usb_driver iPhone_driver = {
@@ -21,3 +31,7 @@ static struct usb_driver iPhone_driver = {
     .disconnect = i_disconnect,
     .id_table = id_table
 }
+
+module_init(i_init);
+module_exit(i_exit);
+
