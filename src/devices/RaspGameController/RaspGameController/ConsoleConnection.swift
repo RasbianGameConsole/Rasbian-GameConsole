fcc78003cfdb7643955ace27b269a0c2a623b519
Edward Hubbard <edwardhubbard@Edwards-MacBook-Air.local>
2018-12-27 02:46:02 -0600

    iOS APIfor MQTT Connection

---
+++ src/devices/RaspGameController/RaspGameController/ConsoleConnection.swift
@@ -0,0 +1,132 @@
+//
+//  ConsoleConnection.swift
+//  RaspGameController
+//
+//  Created by Edward Hubbard on 12/27/18.
+//  Copyright © 2018 Edward Hubbard. All rights reserved.
+//
+
+import Foundation
+import CocoaMQTT
+
+class ConsoleConnection : CocoaMQTTDelegate {
+
+    
+    //SINGLETON LOGIC==========================================
+    
+    private static var connection : ConsoleConnection? = nil
+
+    public static func shared() -> ConsoleConnection{
+        if(ConsoleConnection.connection == nil){
+            ConsoleConnection.connection = ConsoleConnection()
+        }
+        return ConsoleConnection.connection!
+    }
+    
+    //MEMBER VARS==============================================
+    
+    private var MQTTClient : CocoaMQTT? = nil
+    private var connected : Bool = false
+    private var connectionTopic : String = "connect/" + (UIDevice.current.identifierForVendor?.uuidString)!
+    private var currentData : String? = nil
+    private var players : [Int]? = nil
+    
+    //INIT's===================================================
+    
+    private init(){
+        let phoneID =  UIDevice.current.identifierForVendor?.uuidString
+        self.MQTTClient = CocoaMQTT(clientID: phoneID!, host: "ENTER PI IP HERE", port: 1883)
+        self.MQTTClient?.delegate = self
+        do {
+            try self.connect()
+        } catch let err as NSError? {
+            print(err?.domain as Any) // TODO: REPLACE PRINT STATEMENTS WITH USER USEFUL ERROR HANDLING
+            print("FAILED TO CONNECT")
+        }
+    }
+    
+    private func connect() throws ->  NSError?{
+        if(MQTTClient?.connect() == false){
+            throw NSError(domain: "Cannot Connect to Device", code: 1, userInfo: nil)
+        }
+        self.connected = true
+        return nil
+    }
+    
+    private func availablePlayerSlots(slots: String){
+        
+        let strArr = slots.split(separator: "-")
+        var iArr = Array<Int>()
+        for i in strArr{
+            iArr.append(Int(i)!)
+        }
+        self.players = iArr;
+    }
+    
+    private func getData(data: String){
+        self.currentData = data
+    }
+    
+    //API ACCESSIBLE FUNCTIONS=====================================
+    
+    public func disconnect(){
+        self.MQTTClient?.disconnect()
+    }
+    
+    public func publishData(topic: String, data: String){
+        self.MQTTClient?.publish(CocoaMQTTMessage(topic: topic, string: data))
+    }
+    
+    
+    public func getClient() -> CocoaMQTT?{     //USE AT YOUR OWN RISK
+        return self.MQTTClient
+    }
+    
+    public func getAvailPlayers() -> [Int]{
+        return self.players!
+    }
+    
+    // DELEGATE PROTOCOL METHODS============================================
+    
+    func mqtt(_ mqtt: CocoaMQTT, didConnectAck ack: CocoaMQTTConnAck) {
+        self.MQTTClient?.subscribe(self.connectionTopic)
+        print("connected")
+    }
+    
+    func mqtt(_ mqtt: CocoaMQTT, didPublishMessage message: CocoaMQTTMessage, id: UInt16) {
+        print("published")
+        print(message.string)
+    }
+    
+    func mqtt(_ mqtt: CocoaMQTT, didPublishAck id: UInt16) {
+        
+    }
+    
+    func mqtt(_ mqtt: CocoaMQTT, didReceiveMessage message: CocoaMQTTMessage, id: UInt16) {
+        if(message.topic == self.connectionTopic){
+            availablePlayerSlots(slots: message.string!)
+        }else{
+            self.getData(data: message.string!)
+        }
+    }
+    
+    func mqtt(_ mqtt: CocoaMQTT, didSubscribeTopic topic: String) {
+        print("subscribed to \(topic)")
+    }
+    
+    func mqtt(_ mqtt: CocoaMQTT, didUnsubscribeTopic topic: String) {
+        print("unsubscribed from \(topic)")
+    }
+    
+    func mqttDidPing(_ mqtt: CocoaMQTT) {
+        
+    }
+    
+    func mqttDidReceivePong(_ mqtt: CocoaMQTT) {
+        
+    }
+    
+    func mqttDidDisconnect(_ mqtt: CocoaMQTT, withError err: Error?) {
+        
+    }
+}
--- src/devices/RaspGameController/RaspGameController/ViewController.swift
+++ src/devices/RaspGameController/RaspGameController/ViewController.swift
@@ -7,28 +7,50 @@
 //
 
 import UIKit
-import ExternalAccessory
+import CocoaMQTT
+import Foundation
 
 class ViewController: UIViewController {
 
+    var mqttClient : ConsoleConnection? = nil
+    var label : UILabel? = nil
+    static var incr : Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        let phoneID = UIDevice.current.identifierForVendor?.uuidString
+        self.mqttClient = ConsoleConnection.shared()
+        self.label = textView(txt: "finding...")
+        let btn : UIButton = findBtn()
+        
+        self.view.addSubview(btn)
         self.view.backgroundColor = UIColor.green
-        let accessory = EAAccessoryManager.shared()
-        textView(acc: accessory.connectedAccessories)
-        /*let EA = EASession(accessory: .init(), forProtocol: "")
-        var str = "hello"
-        let array: [UInt8] = Array(str.utf8)
-        EA!.outputStream?.write(array, maxLength: 5)*/
-        // Do any additional setup after loading the view, typically from a nib.
     }
     
-    func textView(acc: [EAAccessory]){
-        let text = UILabel()
-        text.text = acc[0].name
+    func textView(txt: String) -> UILabel{
+        let rect = CGRect(x: 100, y: 100, width: 100, height: 200)
+        let text = UILabel(frame: rect)
+        text.text = txt
         self.view.addSubview(text)
-        
+        return text
     }
+    
+    func findBtn() -> UIButton{
+        let  rect = CGRect(x: 100, y: 250, width: 100, height: 200)
+        let btn = UIButton(frame: rect)
+        btn.backgroundColor = .red
+        btn.setTitle("find/unfind pi", for: .normal)
+        btn.addTarget(self, action: #selector(callback), for: .touchUpInside)
+        return btn
+    }
+    
+    @objc func callback(){
+        label?.text = String(ViewController.incr)
+        self.mqttClient?.publishData(topic: "TEST", data: (label?.text)!)
+       ViewController.incr += 1
+    }
+    
+    
 
 
 }
